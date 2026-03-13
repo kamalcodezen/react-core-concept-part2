3a9cacac22508080d909d419ffb14e352e6aa6f1
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-14 03:27:41 +0530

    hide and show toggle system practice in usestate

--- src/hide_show_text.jsx
+++ src/hide_show_text.jsx
@@ -1,7 +1,20 @@
- export default function ShowHideText() {
+import { use, useState } from "react";
+
+export default function ShowHideText() {
+  const [show, setShow] = useState(false);
+
+  const toggleHandle = () => {
+    setShow(show === false);
+  };
+
   return (
     <div className="users">
-      <h1>Show And Hide Text</h1>
+      <h1>Show And Hide Text </h1>
+      {show && <p className="counterBtn">Hello, React Leaner</p>}
+
+      <button onClick={toggleHandle} className="counterBtn">
+        {show ? "Hide" : "Show"}
+      </button>
     </div>
   );
 }
