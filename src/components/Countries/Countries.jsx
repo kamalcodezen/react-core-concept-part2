8013163ea95784a56e10d43c333a17fddc5dfd16
kamalcodezen <kamaluddin7908@gmail.com>
2026-04-21 14:26:09 +0530

    css add and button add in country card

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { use } from "react";
 import Country from "../Country/Country";
+import "./Countries.css"
 
 const Countries = ({ countriesFetch }) => {
   const countriesData = use(countriesFetch);
@@ -9,9 +10,12 @@ const Countries = ({ countriesFetch }) => {
   return (
     <div>
       <h1>Countries : {countries.length}</h1>
-      {countries.map((country) => (
-        <Country key={country.ccn3.ccn3} country={country}></Country>
-      ))}
+
+      <div className="countries">
+        {countries.map((country) => (
+          <Country key={country.ccn3.ccn3} country={country}></Country>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React from "react";
 
-const Country = ({country}) => {
-    console.log(country)
-    return (
-        <div>
-            <p>Name : {country.name.common}</p>
-            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
-        </div>
-    );
+const Country = ({ country }) => {
+  console.log(country);
+
+  const handleVisited = () => {
+    console.log("button was clicked");
+  };
+
+  const countryCss = {
+    border: "1px solid cyan",
+    // margin: "20px",
+    padding: "20px",
+    borderRadius: "10px",
+    display: "flex",
+    flexDirection: "column",
+    // alignItem: "center",
+    // justifyContent: "center",
+    gap: "10px",
+  };
+
+  return (
+    <div style={countryCss}>
+      <img
+        style={{ height: "100px", width: "200px", marginInline: "auto" }}
+        src={country.flags.flags.png}
+        alt={country.flags.flags.alt}
+      />
+      <p>Name : {country.name.common}</p>
+      <p>Region :{country.region.region}</p>
+      <button className="country-btn" onClick={handleVisited}>
+        Clicked me
+      </button>
+    </div>
+  );
 };
 
 export default Country;
